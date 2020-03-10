61b1254a724610c1954d00714c65ac1785e0966e
jmichel80 <mail@julienmichel.net>
2020-03-10 18:23:34 +0000

    added horizontal line to separate epochs

--- prettyplot.py
+++ prettyplot.py
@@ -18,12 +18,21 @@ for y in range(0,nruns):
     y_vals.append([])
 y_avg = []
 error_vals = []
+
+epoch_step = 9
+epoch_counter = 1
+bars = []
 for line in buffer:
     if line.startswith("#"):
         continue
     elems = line.split()
     ctime = float(elems[0])
     x_vals.append(ctime)
+    epoch_counter += 1
+    if (epoch_counter > epoch_step):
+        bars.append(ctime)
+        #print ctime
+        epoch_counter = 0
     for runid in range(0,nruns):
         y = float(elems[runid+1])
         y_vals[runid].append(y)
@@ -38,6 +47,9 @@ for entry in y_vals:
 plt.ylabel('$\Delta \it{G}$ / kcal.mol$^-$$^1$')
 plt.xlabel('Cumulative sampling time / ns')
 plt.fill_between(x_vals, np.array(y_avg)-np.array(error_vals), np.array(y_avg)+np.array(error_vals), alpha=0.5, facecolor='#ffa500' )
+
+for b in bars:
+    plt.axvline(x=b,c='b', linestyle='dotted')
 plt.savefig(figfile)
 plt.show()
 
